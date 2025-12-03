574dfbd2f111a6bb25b2db9cc8447390775815ae
Amélie B. <sashell.fx@gmail.com>
2025-12-03 19:31:01 +0100

    Day 2

---
+++ Day 2/PatternDetector.cs
@@ -0,0 +1,121 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Day_2
+{
+	public class PatternDetector
+	{
+		private List<string> _allIDs = new();
+
+
+		public PatternDetector()
+		{
+			var input = File.ReadAllText("input.txt");
+			foreach (var range in input.Split(','))
+			{
+				var bounds = range.Split('-');
+				for (long i = (long)Convert.ToDouble(bounds[0]); i <= (long)Convert.ToDouble(bounds[1]); i++)
+				{
+					_allIDs.Add(i.ToString());
+				}
+			}
+		}
+
+		public long CalculateCode()
+		{
+			long code = 0;
+			foreach (var id in _allIDs)
+			{
+				if (IsIdInvalid(id))
+				{
+					code += (long)Convert.ToDouble(id);
+				}
+			}
+
+			return code;
+		}
+
+		public long CalculateCodePartTwo()
+		{
+			long code = 0;
+
+			foreach (var id in _allIDs)
+			{
+				if (IsIdInvalidPartTwo(id))
+				{
+					code += (long)Convert.ToDouble(id);
+				}
+			}
+
+			return code;
+		}
+
+		private bool IsIdInvalid(string id)
+		{
+			string pattern = id[0].ToString();
+			var regex = new Regex(pattern);
+
+			//ID in only the same digit
+			if (regex.Matches(id).Count == id.Length && id.Length%2 == 0)
+			{
+				return true;
+			}
+
+			int i = 0;
+			foreach (char digit in id[1..])
+			{
+				i++;
+				if (digit.Equals(pattern[0]))
+				{
+					if (id[i..].Equals(pattern))
+					{
+						return true;
+					}
+					else
+					{
+						pattern += digit;
+					}
+				}
+				else
+				{
+					pattern += digit;
+				}
+			}
+
+			return false;
+		}
+
+		private bool IsIdInvalidPartTwo(string id)
+		{
+			int length = id.Length;
+
+			for (int patternLen = 1; patternLen <= length / 2; patternLen++)
+			{
+				if (length % patternLen != 0)
+				{
+					continue;
+				}
+
+				string pattern = id.Substring(0, patternLen);
+				int repeats = length / patternLen;
+
+				bool isMatch = true;
+				for (int i = 1; i < repeats; i++)
+				{
+					string segment = id.Substring(i * patternLen, patternLen);
+					if (segment != pattern)
+					{
+						isMatch = false;
+						break;
+					}
+				}
+
+				if (isMatch && repeats >= 2)
+				{
+					return true;
+				}
+			}
+
+			return false;
+		}
+	}
+}
--- Main/Program.cs
+++ Main/Program.cs
@@ -1,4 +1,5 @@
 ﻿using Day_1;
+using Day_2;
 
 namespace Advent_of_Code_2025;
 
@@ -9,9 +10,9 @@ public class AOC25
 		DateTime start;
 		double timer = 0;
 
-		Console.WriteLine("/////////// 01 /////////// \r\n");
-
 		start = DateTime.Now;
+
+		/*Console.WriteLine("/////////// 01 /////////// \r\n");
 
 		var dial = new SafeDial(50);
 		var result = dial.PlaySequenceAndCount(0);
@@ -24,6 +25,21 @@ public class AOC25
 		var result2 = dial.PlaySequenceAndCount(0, true);
 
 		Console.WriteLine($"CODE : {result2}" + "\r\n");
+
+		timer = (DateTime.Now - start).TotalMilliseconds;
+		Console.WriteLine($"Time : {timer} ms" + "\r\n");*/
+
+		Console.WriteLine("/////////// 02 /////////// \r\n");
+
+		var detector = new PatternDetector();
+		var code = detector.CalculateCode();
+
+		Console.WriteLine($"CODE IS {code}");
+
+		Console.WriteLine("/////////// 02 /////////// \r\n");
+
+		var codePartTwo = detector.CalculateCodePartTwo();
+		Console.WriteLine($"CODE PART TWO IS {codePartTwo}");
 
 		timer = (DateTime.Now - start).TotalMilliseconds;
 		Console.WriteLine($"Time : {timer} ms" + "\r\n");
